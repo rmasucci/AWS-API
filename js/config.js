f86a02bcd838d2b199468e6fd9999cad914bee16
rmasucci <rmm1404@hotmail.com>
2025-01-02 18:36:18 -0500

    updated config

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 const config = {
-    userPoolId: 'us-east-1_xxxxxx', // Your User Pool ID
-    clientId: 'xxxxxxxxxxxxxxxxxx',  // Your App Client ID
-    domain: 'your-domain-prefix.auth.us-east-1.amazoncognito.com',
+    userPoolId: 'us-east-1_nfpYH8ZRYx', // Your User Pool ID
+    clientId: '6sd335m3c12tt510u3eu4ve4du',  // Your App Client ID
+    domain: 'us-east-1nfpyh8zry.auth.us-east-1.amazoncognito.com',
     region: 'us-east-1',
-    apiEndpoint: 'https://your-api-gateway-url/prod/calculate',
-    redirectUri: 'https://your-cloudfront-domain/callback.html',
-    postLogoutRedirectUri: 'https://your-cloudfront-domain/index.html'
+    apiEndpoint: 'https://9909whh55j.execute-api.us-east-1.amazonaws.com/prod/calculate',
+    redirectUri: 'https://dl3afkou5pqln.cloudfront.net/callback.html',
+    postLogoutRedirectUri: 'https://dl3afkou5pqln.cloudfront.net/index.html'
 };
