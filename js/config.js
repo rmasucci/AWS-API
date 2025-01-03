96768c47beb8716c2b2b21a7c8c9edfeb8658823
rmasucci <rmm1404@hotmail.com>
2025-01-03 12:51:15 -0500

    updated config with domain and auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const config = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nfpYH8ZRY",
+    authority: 'https://calculator-app-test.auth.us-east-1.amazoncognito.com',
+    domain: 'calculator-app-test.auth.us-east-1.amazoncognito.com',
     userPoolId: 'us-east-1_nfpYH8ZRY', // Your User Pool ID
     clientId: '6sd335m3c12tt510u3eu4ve4du',  // Your App Client ID
-    domain: 'us-east-1nfpyh8zry.auth.us-east-1.amazoncognito.com',
     region: 'us-east-1',
     apiEndpoint: 'https://9909whh55j.execute-api.us-east-1.amazonaws.com/prod/calculate',
     redirectUri: 'https://dl3afkou5pqln.cloudfront.net/callback.html',
