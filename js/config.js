444f975389de4d2ff7b005c7f644843efaadc881
rmasucci <rmm1404@hotmail.com>
2025-01-03 16:55:03 -0500

    updated config with new authority in config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const config = {
-    authority: 'https://calculator-app-test.auth.us-east-1.amazoncognito.com',
-    domain: 'calculator-app-test.auth.us-east-1.amazoncognito.com',
+    authority: 'https://us-east-1nfpyh8zry.auth.us-east-1.amazoncognito.com',
+    domain: 'https://us-east-1nfpyh8zry.auth.us-east-1.amazoncognito.com',
     userPoolId: 'us-east-1_nfpYH8ZRY', // Your User Pool ID
     clientId: '6sd335m3c12tt510u3eu4ve4du',  // Your App Client ID
     region: 'us-east-1',
